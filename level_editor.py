c1b3f2bb78186aeab7d6ea0269239e3ac582004e
0Nikola0 <62570597+0Nikola0@users.noreply.github.com>
2020-09-09 18:43:15 +0200

    Update level_editor.py
    Added 3 states of the tiles and created save function

--- level_editor.py
+++ level_editor.py
@@ -29,6 +29,19 @@ class Tiles:
         """
 
 
+def save_tiles(s_tiles: list, tiles_type: str, lvl: str):
+    # Level folder must exist before saving the tiles in selected folder
+    """
+    s_tiles ex: tiles1[]
+    tiles_type ex: ladders
+    lvl ex: 01
+    """
+    # Saves the pos of the occupied type X tiles
+    with open(f"level{lvl}/{tiles_type}.tiles", "w") as file:
+        for s_tile in s_tiles:
+            file.write(str(s_tile) + ", ")
+
+
 White = (255, 255, 255)
 Gray = (50, 50, 50)
 Red = (255, 0, 0)
@@ -48,6 +61,9 @@ for ver in range(12):
     tile_posy += tile_size   # Changing the y pos to the next line
     tile_posx = 0     # Resetting the x pos so it is from the starting position again
 
+tiles1 = []
+tiles2 = []
+tiles3 = []
 
 running = True
 while running:
@@ -57,6 +73,27 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             for tile in tiles:
                 tile.clicked(event)
+         # If "S" pressed on keyboard it will save all selected tiles in .lvl file
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                print("Clicked S(ave)")
+                for tile in tiles:
+                    # Append tiles pos to correct list if tile is occupied
+                    if not tile.available:
+                        if tile.type == 1:
+                            tiles1.append(tile.pos)
+                        elif tile.type == 2:
+                            tiles2.append(tile.pos)
+                        else:
+                            tiles3.append(tile.pos)
+                save_tiles(tiles1, tiles_type="dirt", lvl="01")
+                save_tiles(tiles2, tiles_type="ladders", lvl="01")
+                save_tiles(tiles3, tiles_type="lava", lvl="01")
+                # Flash white screen when level is saved
+                screen.fill(White)
+                pygame.display.flip()
+                print("Saved")
+
     
     screen.fill(Gray)
     for tile in tiles:
